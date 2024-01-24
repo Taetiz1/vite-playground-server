7f9005a134f053b20083b670f143414e0ce0d012
Taetiz1 <120712765+Taetiz1@users.noreply.github.com>
2024-01-24 11:59:21 +0700

    fixed

--- server.js
+++ server.js
@@ -1,8 +1,3 @@
-// import fs from 'fs'
-// import express from 'express'
-// import Router from 'express-promise-router'
-// import { createServer } from 'vite'
-// import viteConfig from '../vite.config.js'
 import { Server } from 'socket.io';
 import editJsonFile from 'edit-json-file'
  
@@ -21,43 +16,6 @@ const adminData = editJsonFile('./admin_log.json', {
 const roomData = editJsonFile('./rooms.json', {
     autosave: true
 });
-
-// // Create router
-// const router = Router()
-
-// // Create vite front end dev server
-// const vite = await createServer({
-//     configFile: false,
-//     server: {
-//         middlewareMode: 'html',
-//     },
-//     ...viteConfig,
-// })
-
-// // Main route serves the index HTML
-// router.get('/', async (req, res, next) => {
-//     let html = fs.readFileSync('./index.html', 'utf-8')
-//     html = await vite.transformIndexHtml(req.url, html)
-//     res.send(html)
-// })
-
-// // Use vite middleware so it rebuilds frontend
-// router.use(vite.middlewares)
-
-// // Everything else that's not index 404s
-// router.use('*', (req, res) => {
-//     res.status(404).send({ message: 'Not Found' })
-// })
-
-// // Create express app and listen on port 
-// const app = express()
-// app.use(router)
-
-// const port = process.env.PORT || 3000;
-
-// const server = app.listen(port, () => {
-//     console.log(`Listening on port http://localhost:${port}`)
-// })
 
 const origin = process.env.CLIENT_URL || "http://localhost:5173";
 
@@ -93,6 +51,7 @@ const loadRooms = async () => {
         const room = {
         ...roomItem,
         clients: {},
+        activeVoice: []
         };
 
         rooms.push(room);
@@ -111,45 +70,37 @@ ioServer.on('connection', (client) => {
         `User ${client.id} connected, there are currently ${ioServer.engine.clientsCount} users connected`
     )
 
-    client.on('joinroom', ({id, name, avatarUrl, email, roomID}) => {
+    client.on('joinroom', ({id, name, avatarUrl, roomID}) => {
 
-        if(!rooms[roomID].clients.hasOwnProperty(id)) {
-            rooms[roomID].clients[id] = {
-                name: name,
-                position: [0, 0, 0],
-                rotation: [0, 0, 0],
-                action: "idle",
-                chathead: "" ,
-                avatarUrl: avatarUrl,
-                // equipment: [],
-                // config: {
-                //     Skin: "#E7C495",
-                //     Pupil: "#000000",
-                //     Iris: "#798ABC",
-                //     Sclera: "#FFFFFF",
-                //     Hair: "hair_01",
-                //     HairColor: "#25262b",
-                // },
-                // inventory: {},
-                // email: email,
+        try {
+            if(!rooms[roomID].clients.hasOwnProperty(id)) {
+                rooms[roomID].clients[id] = {
+                    name: name,
+                    position: [0, 0, 0],
+                    rotation: [0, 0, 0],
+                    action: "idle",
+                    chathead: "" ,
+                    avatarUrl: avatarUrl,
+                }
+    
+                if(clients[id].currentRoom === ''){
+    
+                    clients[id].currentRoom = roomID
+    
+                } else {
+                    
+                    delete rooms[clients[id].currentRoom].clients[id]
+                    clients[id].currentRoom = roomID
+                }
+    
+                client.emit('respawn', [0, 5, 0])
+                client.emit('move', rooms[roomID].clients)
+                client.emit('currentRoom', roomID)
             }
-
-            if (clients[id].currentRoom === ''){
-                clients[id].currentRoom = roomID
-                // clients[id] = {
-                //     currentRoom: roomID,
-                //     email: email,
-                // }
-
-            } else {
-                
-                delete rooms[clients[id].currentRoom].clients[id]
-                clients[id].currentRoom = roomID
-            }
-
-            client.emit('respawn', [0, 5, 0])
-            client.emit('move', rooms[roomID].clients)
-            client.emit('currentRoom', roomID)
+        } catch(error) {
+            client.disconnect(); 
+            console.error(error);
+            console.log(`user ${client.id} disconnected`)
         }
         
     })
@@ -187,77 +138,22 @@ ioServer.on('connection', (client) => {
         }
     
     })
-
-    // client.emit('inventory', clients[client.id].inventory)
-
-    // client.on('equipItem', ({id, item}) => {
-    //     clients[id].inventory[item].equipped = !clients[id].inventory[item].equipped
-    //     if(clients[id].inventory[item].equipped){
-    //         if (!clients[id].equipment.includes(item)) {
-    //             clients[id].equipment.push(item)
-    //         }
-    //     } else {
-    //         clients[id].equipment = clients[id].equipment.filter((member) => member !== item);
-    //     }
-
-        
-    //     client.emit('inventory', clients[id].inventory)
-    // })
-
-    // client.on('checkItem', ({item}) => {
-    //     clients[client.id].inventory[item].isNew = false
-
-    //     client.emit('inventory', clients[client.id].inventory)
-    // })
-
-    // client.on('collectItem', ({item}) => {
-    //     let type, header, text;
-    //     if(!clients[client.id].inventory.hasOwnProperty(item.id)){
-    //         const setItem = {
-    //             [item.id] : {
-    //                 name: item.name,
-    //                 image: item.image, 
-    //                 equipped: item.equipped, 
-    //                 isNew: item.isNew
-    //             }
-    //         }
-
-    //         clients[client.id].inventory = { ...clients[client.id].inventory, ...setItem }
-    //         client.emit('inventory', clients[client.id].inventory)
-    //         type = "normal"
-    //         header = "ยินดีด้วย"
-    //         text = `คุณได้รับเครื่องประดับชิ้นใหม่! ${item.name} ลองตรวจสอบใน inventory สิ`
-            
-    //         client.emit('isPicked', {type, header, text})
-
-    //     } else {
-            
-    //         const type = "error"
-    //         const header = "ล้มเหลม"
-    //         const text = "ว่างเปล่า! คุณเคยเก็บไอเท็มนี้ไปแล้ว"
-
-    //         client.emit('isPicked', {type, header, text})
-    //     }
-
-        
-    // })
     
     client.on('move', ({ id, rotation, position, action }) => {
-        rooms[clients[id].currentRoom].clients[id].position = position
-        rooms[clients[id].currentRoom].clients[id].rotation = rotation
-        rooms[clients[id].currentRoom].clients[id].action = action
+        try {
+            
+            rooms[clients[id].currentRoom].clients[id].position = position
+            rooms[clients[id].currentRoom].clients[id].rotation = rotation
+            rooms[clients[id].currentRoom].clients[id].action = action
 
-        client.emit('move', rooms[clients[id].currentRoom].clients)
+            client.emit('move', rooms[clients[id].currentRoom].clients)
+
+        } catch (error) {
+            client.disconnect(); 
+            console.error(error);
+            console.log(`user ${client.id} disconnected`)
+        }
     })
-
-    // client.on('config', ({ id, Skin, Pupil, Iris, Sclera, Hair, HairColor }) => {
-    //     clients[id].config.Skin = Skin
-    //     clients[id].config.Pupil = Pupil
-    //     clients[id].config.Iris = Iris
-    //     clients[id].config.Sclera = Sclera
-    //     clients[id].config.Hair = Hair
-    //     clients[id].config.HairColor = HairColor
-    // })
 
     client.on('config', ({ id, avatarUrl}) => {
         rooms[clients[id].currentRoom].clients[id].avatarUrl = avatarUrl
@@ -269,7 +165,7 @@ ioServer.on('connection', (client) => {
             
         rooms[clients[msg.id].currentRoom].clients[msg.id].chathead = msg.message;
 
-        if (chatheadTimeout) {
+        if(chatheadTimeout) {
             clearTimeout(chatheadTimeout);
         }
 
@@ -279,6 +175,24 @@ ioServer.on('connection', (client) => {
         
         ioServer.sockets.emit('message', messages);
     });
+
+    client.on('join voice', () => {
+
+        try {
+
+            client.emit('call users', rooms[clients[client.id].currentRoom].activeVoice)
+
+            if(!rooms[clients[client.id].currentRoom].activeVoice.hasOwnProperty(client.id)) { 
+            
+                rooms[clients[client.id].currentRoom].activeVoice.push(client.id)
+            }
+
+        } catch (error) {
+            client.disconnect(); 
+            console.error(error);
+            console.log(`user ${client.id} disconnected`)
+        }
+    })
 
     // client.emit("selectedQuestions", randomQuestions());
 
@@ -350,9 +264,6 @@ ioServer.on('connection', (client) => {
                 if (index !== -1) {
                     activeEmail.splice(index, 1);
                 }
-
-                // database.pop(`${email}.inventory`)
-                // database.append(`${email}.inventory`, clients[client.id].inventory);
             }
             
 
